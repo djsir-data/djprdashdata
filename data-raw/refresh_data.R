673aa0f957f6a1da30b2ebfad68bda8dbb42d268
Matt Cowgill <mattcowgill@gmail.com>
2021-04-23 15:27:52 +1000

    format update time; add WPI

--- data-raw/refresh_data.R
+++ data-raw/refresh_data.R
@@ -1,9 +1,10 @@
 pkgload::load_all()
 
 abs_6202 <- read_abs_if_updated(cat_no = "6202.0")
+abs_6345 <- read_abs_if_updated(cat_no = "6345.0")
 
-last_refreshed <- Sys.time()
-
+# Save file containing time that this script was last run
+last_refreshed <- format(Sys.time(), tz = "Australia/Melbourne")
 file_conn <- file(here::here("data-raw", "last_refreshed.txt"))
 writeLines(as.character(Sys.time()), file_conn)
 close(file_conn)
