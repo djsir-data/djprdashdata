d009cdef09055af4df3cb963d720024ecc50dc1c
Matt Cowgill <mattcowgill@gmail.com>
2021-05-25 14:00:13 +1000

    add dl_and_unzip

---
+++ R/dl_and_unzip.R
@@ -0,0 +1,52 @@
+#' Download a ZIP file from the ABS and unzip the contents
+#'
+#' @param catalogue_string Such as "labour-force-australia" or "labour-force-australia-detailed".
+#' See `?readabs::search_files()` for more.
+#' @param dest_dir Path to directory to which ZIP contents should be extracted
+#'
+#' @examples
+#' \dontrun{
+#' dl_and_unzip("labour-force-australia")
+#' }
+#'
+
+dl_and_unzip <- function(catalogue_string,
+                         dest_dir = file.path("data-raw", "raw-data", catalogue_string)
+                         ) {
+
+  temp_dir <- tempdir()
+  on.exit(unlink(temp_dir))
+
+  # Make sure to reset user's timeout option when function exits
+  user_timeout <- getOption("timeout")
+  on.exit(options("timeout" = user_timeout))
+  # Set per-file timeout in seconds
+  options("timeout" = 5 * 60)
+
+  # Get fragments of filenames to download
+  file_basenames <- readabs::search_files(string = ".zip",
+                                catalogue = catalogue_string) %>%
+    basename()
+
+  # Download files to temp_dir
+  files <- purrr::map_chr(.x = file_basenames,
+                 .f = readabs::download_abs_data_cube,
+                 catalogue_string = catalogue_string,
+                 path = temp_dir)
+
+  is_pivot <- grepl("pivot", file_basenames, ignore.case = TRUE)
+  is_pivot <- ifelse(is_pivot, "pivot", "time-series")
+
+  dest_dirs <- file.path(dest_dir, is_pivot)
+
+  purrr::walk2(.x = files,
+               .y = dest_dirs,
+               .f = ~zip::unzip(zipfile = .x,
+                                overwrite = TRUE,
+                                junkpaths = FALSE,
+                                exdir = .y)
+  )
+
+  return(TRUE)
+}
+
---
+++ tests/testthat/test-dl_and_unzip.R
@@ -0,0 +1,25 @@
+test_that("dl_and_unzip downloads and unzips", {
+  temp_excel_dir <- tempdir()
+  on.exit(unlink(temp_excel_dir))
+  ts_dir <- file.path(temp_excel_dir, "time-series")
+
+  dl_and_unzip("labour-force-australia",
+               dest_dir = temp_excel_dir)
+
+  ts_files <- list.files(ts_dir, full.names = TRUE)
+
+  # Test Excel files exist
+  expect_true(dir.exists(ts_dir))
+  expect_true(all(tools::file_ext(ts_files) == "xls"))
+
+  # Test Excel files are non-zero size
+  expect_gt(min(file.size(ts_files)), 50000)
+
+  # Test time series spreadsheets are in expected form
+  tidy_ts <- readabs::read_abs_local(filenames = list.files(ts_dir),
+                                     path = ts_dir)
+
+  expect_s3_class(tidy_ts, "tbl")
+  expect_gt(nrow(tidy_ts), 2000000)
+  expect_length(tidy_ts, 12)
+})
